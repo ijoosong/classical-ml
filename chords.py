d5db3ca99257824c1e5e12c6ceaba050dbc2394b
joe_song <jsong@gmail.com>
2016-01-23 23:12:43 -0500

    stuff

--- chords.py
+++ chords.py
@@ -3,6 +3,30 @@ from listFunctions import cumulativeSumZero
 
 def getPitches(intervals):
     return cumulativeSumZero(intervals)
+major = [4, 3, 5]
+major7 = [4, 3, 4]
+major65 = [3, 4, 1]
+major43 = [4, 1, 4]
+major42 = [1, 4, 3]
+major6 = [3, 5]
+major64 = [5, 4, 3]
+minor = [3, 4, 5]
+minor7 = [3, 4, 3]
+minor65 = [4, 3, 2]
+minor43 = [3, 2, 3]
+minor42 = [2, 3, 4]
+minor6 = [4, 5]
+dom = major
+dom7 = [4, 3, 3]
+dom65 = [3, 3, 2]
+dom43 = [3, 2, 4]
+dom42 = [2, 4, 3]
+dom6 = major6
+dim = [3, 3, 6]
+dimmaj7 = [3, 3, 5]
+dimmin7 = [3, 3, 4]
+dimfull7 = [3, 3, 3]
+
 chords = {'I': [4, 3, 5], 'I7': [4, 3, 4], 'I6': [3, 5], 'I64': [5, 4, 3], 'I43': [3, 4, 1], 'I42': [4, 1, 4],
           'ii': [3, 4, 5], 'ii7': [3, 4, 3], }
 
